e3ac10c7f115d16e3c5baa78282471e9287a6762
jurvantoledo <jurvantoledo@live.nl>
2020-12-01 11:25:08 +0100

    added delete function for products

--- src/store/storeDetails/actions.js
+++ src/store/storeDetails/actions.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 import { apiUrl } from "../../config/constants";
+import { getUserWithStoredToken } from "../user/actions";
+import { selectToken } from "../user/selectors";
 
 export const STORE_DETAILS_FETCHED = "STORE_DETAILS_FETCHED";
+export const PRODUCT_DELETE_SUCCESS = "RESERVATION_DELETE_SUCCESS";
+
+export const productDeleteSuccess = userId => ({
+  type: PRODUCT_DELETE_SUCCESS,
+  payload: userId
+});
 
 const storeDetailsFetched = store => ({
   type: STORE_DETAILS_FETCHED,
@@ -13,5 +21,26 @@ export const fetchStoreById = id => {
     const response = await axios.get(`${apiUrl}/store/${id}`);
     console.log(response);
     dispatch(storeDetailsFetched(response.data.store));
+  };
+};
+
+export const deleteProduct = (id) => {
+  return async (dispatch, getState) => {
+    const token = selectToken(getState());
+
+    if (token === null) return;
+    try {
+      const response = await axios.delete(`${apiUrl}/product/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      console.log("this is response", response);
+      dispatch(getUserWithStoredToken());
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.message);
+      } else {
+        console.log(error);
+      }
+    }
   };
 };
