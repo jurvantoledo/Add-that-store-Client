bce4d22a7640d485a9da408abd774ee4ac532a74
jurvantoledo <jurvantoledo@live.nl>
2020-12-03 21:55:03 +0100

    changed route in navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -7,11 +7,14 @@ import { selectToken, selectUser } from "../../store/user/selectors";
 import NavbarItem from "./NavbarItem";
 import LoggedIn from "./LoggedIn";
 import LoggedOut from "./LoggedOut";
+import { selectUserInfo } from "../../store/userInfo/selectors";
+
 
 export default function Navigation() {
   const token = useSelector(selectToken);
-  const { id } = useSelector(selectUser)
-  const { isOwner }  = useSelector(selectUser)
+  const { id, user, isOwner } = useSelector(selectUser)
+  const userInfo = useSelector(selectUserInfo)
+
 
   const loginLogoutControls = token ? <LoggedIn /> : <LoggedOut />;
 
@@ -24,7 +27,7 @@ export default function Navigation() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav style={{ width: "100%" }} fill>
           <NavbarItem path="/" linkText="Home" />
-          {isOwner ? <NavbarItem path={`/info/${id}`} linkText="User Info" /> : null}
+          { userInfo.id === user.id && isOwner ? <NavbarItem path={`/info/${id}`} linkText="User Info" /> : null}
           {loginLogoutControls}
         </Nav>
       </Navbar.Collapse>
