42f459b165f37fd0172541cd69f4282d6c46c70a
jurvantoledo <jurvantoledo@live.nl>
2020-12-03 16:53:22 +0100

    fixed setup for netlify

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-export const apiUrl = process.env.REACT_APP_API_URL || "https://add-a-store.herokuapp.com";
+export const apiUrl = process.env.REACT_APP_API_URL || "https://add-a-store.herokuapp.com/store";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const DEFAULT_PAGINATION_LIMIT = 6;
